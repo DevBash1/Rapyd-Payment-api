7c792479121e2d561cdd90ebc89a9703ce5b963e
Benrobo <alumonabenaiah71@gmail.com>
2022-06-29 13:27:28 -0800

    updated major changes.

--- src/config/index.js
+++ src/config/index.js
@@ -1,13 +1,15 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
+export const ACCESS_SECRET = process.env.ACCESS_TOKEN_SECRET;
 
-export const ACCESS_SECRET = process.env.ACCESS_TOKEN_SECRET
+export const REFRESH_SECRET = process.env.REFRESH_TOKEN_SECRET;
 
-export const REFRESH_SECRET = process.env.REFRESH_TOKEN_SECRET
+export const DATABASE_URL = process.env.DATABASE_URL;
 
-export const DATABASE_URL = process.env.DATABASE_URL
+export const MAX_API_REQUEST = process.env.MAX_API_REQUEST_COUNT;
 
-export const MAX_API_REQUEST = process.env.MAX_API_REQUEST_COUNT
+export const RAPYD_SECRET_KEY = process.env.RAPYD_SECRET_KEY;
 
+export const RAPYD_ACCESS_KEY = process.env.RAPYD_ACCESS_KEY;
