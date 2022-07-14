a590798432029225c6324d7b64303dd18b7e3c91
Benrobo <alumonabenaiah71@gmail.com>
2022-07-14 03:15:23 +0100

    updated wallet controller and added rapyd endpoints to config folder.

--- src/controller/wallets.controller.js
+++ src/controller/wallets.controller.js
@@ -4,7 +4,7 @@ const sendResponse = require("../helpers/response")
 const { validateEmail, validatePhonenumber } = require("../utils/validate")
 const { genAccessToken, genRefreshToken } = require("../helpers/token")
 const Fetch = require("../utils/fetch")
-
+const { createPersonalWallet, createCompanyWallet } = require("../config/rapydEndpoints")
 
 class WalletController {
 
